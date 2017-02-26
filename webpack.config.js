7fdbdd556d8162a2122f609dc7a8390c123e3454
Jeremy Kalsow <elektric@users.noreply.github.com>
2017-02-26 13:14:46 -0600

    initial upload

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var debug = process.env.NODE_ENV !== "production";
+debug = true;
+console.log('ENV: ' + process.env.NODE_ENV);
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: {
+      ministryscheduling: "./ministryscheduling.js",
+      code: "./code.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/static/",
+    filename: "[name].min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
